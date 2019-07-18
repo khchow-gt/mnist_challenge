1a392a0c25255bddee3cbcd1eadfb10b297407cb
Ka Ho Chow <khchowad@connect.ust.hk>
2019-07-18 12:48:16 +0800

    refactor

--- run_attack.py
+++ run_attack.py
@@ -1,5 +1,4 @@
-"""Evaluates a model against examples from a .npy file as specified
-   in config.json"""
+"""Evaluates a model against examples from a .npy file as specified in config.json"""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -18,76 +17,73 @@ import numpy as np
 
 from model import Model
 
+
 def run_attack(checkpoint, x_adv, epsilon):
-  mnist = input_data.read_data_sets('MNIST_data', one_hot=False)
+    mnist = input_data.read_data_sets('MNIST_data', one_hot=False)
 
-  model = Model()
+    model = Model()
 
-  saver = tf.train.Saver()
+    saver = tf.train.Saver()
 
-  num_eval_examples = 10000
-  eval_batch_size = 64
+    num_eval_examples = 10000
+    eval_batch_size = 64
 
-  num_batches = int(math.ceil(num_eval_examples / eval_batch_size))
-  total_corr = 0
+    num_batches = int(math.ceil(num_eval_examples / eval_batch_size))
+    total_corr = 0
 
-  x_nat = mnist.test.images
-  l_inf = np.amax(np.abs(x_nat - x_adv))
-  
-  if l_inf > epsilon + 0.0001:
-    print('maximum perturbation found: {}'.format(l_inf))
-    print('maximum perturbation allowed: {}'.format(epsilon))
-    return
+    x_nat = mnist.test.images
+    l_inf = np.amax(np.abs(x_nat - x_adv))
 
-  y_pred = [] # label accumulator
+    if l_inf > epsilon + 0.0001:
+        print('maximum perturbation found: {}'.format(l_inf))
+        print('maximum perturbation allowed: {}'.format(epsilon))
+        return
 
-  with tf.Session() as sess:
-    # Restore the checkpoint
-    saver.restore(sess, checkpoint)
+    y_pred = []  # label accumulator
 
-    # Iterate over the samples batch-by-batch
-    for ibatch in range(num_batches):
-      bstart = ibatch * eval_batch_size
-      bend = min(bstart + eval_batch_size, num_eval_examples)
+    with tf.Session() as sess:
+        # Restore the checkpoint
+        saver.restore(sess, checkpoint)
 
-      x_batch = x_adv[bstart:bend, :]
-      y_batch = mnist.test.labels[bstart:bend]
+        # Iterate over the samples batch-by-batch
+        for ibatch in range(num_batches):
+            bstart = ibatch * eval_batch_size
+            bend = min(bstart + eval_batch_size, num_eval_examples)
 
-      dict_adv = {model.x_input: x_batch,
-                  model.y_input: y_batch}
-      cur_corr, y_pred_batch = sess.run([model.num_correct, model.y_pred],
-                                        feed_dict=dict_adv)
+            x_batch = x_adv[bstart:bend, :]
+            y_batch = mnist.test.labels[bstart:bend]
 
-      total_corr += cur_corr
-      y_pred.append(y_pred_batch)
+            dict_adv = {model.x_input: x_batch,
+                        model.y_input: y_batch}
+            cur_corr, y_pred_batch = sess.run([model.num_correct, model.y_pred], feed_dict=dict_adv)
 
-  accuracy = total_corr / num_eval_examples
+            total_corr += cur_corr
+            y_pred.append(y_pred_batch)
 
-  print('Accuracy: {:.2f}%'.format(100.0 * accuracy))
-  y_pred = np.concatenate(y_pred, axis=0)
-  np.save('pred.npy', y_pred)
-  print('Output saved at pred.npy')
+    accuracy = total_corr / num_eval_examples
+
+    print('Accuracy: {:.2f}%'.format(100.0 * accuracy))
+    y_pred = np.concatenate(y_pred, axis=0)
+    np.save('pred.npy', y_pred)
+    print('Output saved at pred.npy')
+
 
 if __name__ == '__main__':
-  import json
+    import json
 
-  with open('config.json') as config_file:
-    config = json.load(config_file)
+    with open('config.json') as config_file:
+        config = json.load(config_file)
 
-  model_dir = config['model_dir']
+    model_dir = config['model_dir']
 
-  checkpoint = tf.train.latest_checkpoint(model_dir)
-  x_adv = np.load(config['store_adv_path'])
+    checkpoint = tf.train.latest_checkpoint(model_dir)
+    x_adv = np.load(config['store_adv_path'])
 
-  if checkpoint is None:
-    print('No checkpoint found')
-  elif x_adv.shape != (10000, 784):
-    print('Invalid shape: expected (10000,784), found {}'.format(x_adv.shape))
-  elif np.amax(x_adv) > 1.0001 or \
-       np.amin(x_adv) < -0.0001 or \
-       np.isnan(np.amax(x_adv)):
-    print('Invalid pixel range. Expected [0, 1], found [{}, {}]'.format(
-                                                              np.amin(x_adv),
-                                                              np.amax(x_adv)))
-  else:
-    run_attack(checkpoint, x_adv, config['epsilon'])
+    if checkpoint is None:
+        print('No checkpoint found')
+    elif x_adv.shape != (10000, 784):
+        print('Invalid shape: expected (10000,784), found {}'.format(x_adv.shape))
+    elif np.amax(x_adv) > 1.0001 or np.amin(x_adv) < -0.0001 or np.isnan(np.amax(x_adv)):
+        print('Invalid pixel range. Expected [0, 1], found [{}, {}]'.format(np.amin(x_adv), np.amax(x_adv)))
+    else:
+        run_attack(checkpoint, x_adv, config['epsilon'])
--- train.py
+++ train.py
@@ -1,5 +1,4 @@
-"""Trains a model, saving checkpoints and tensorboard summaries along
-   the way."""
+"""Trains a model, saving checkpoints and tensorboard summaries along the way."""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -36,11 +35,10 @@ global_step = tf.contrib.framework.get_or_create_global_step()
 model = Model()
 
 # Setting up the optimizer
-train_step = tf.train.AdamOptimizer(1e-4).minimize(model.xent,
-                                                   global_step=global_step)
+train_step = tf.train.AdamOptimizer(1e-4).minimize(model.xent, global_step=global_step)
 
 # Set up adversary
-attack = LinfPGDAttack(model, 
+attack = LinfPGDAttack(model,
                        config['epsilon'],
                        config['k'],
                        config['a'],
@@ -50,7 +48,7 @@ attack = LinfPGDAttack(model,
 # Setting up the Tensorboard and checkpoint outputs
 model_dir = config['model_dir']
 if not os.path.exists(model_dir):
-  os.makedirs(model_dir)
+    os.makedirs(model_dir)
 
 # We add accuracy and xent twice so we can easily make three types of
 # comparisons in Tensorboard:
@@ -69,51 +67,49 @@ merged_summaries = tf.summary.merge_all()
 shutil.copy('config.json', model_dir)
 
 with tf.Session() as sess:
-  # Initialize the summary writer, global variables, and our time counter.
-  summary_writer = tf.summary.FileWriter(model_dir, sess.graph)
-  sess.run(tf.global_variables_initializer())
-  training_time = 0.0
+    # Initialize the summary writer, global variables, and our time counter.
+    summary_writer = tf.summary.FileWriter(model_dir, sess.graph)
+    sess.run(tf.global_variables_initializer())
+    training_time = 0.0
 
-  # Main training loop
-  for ii in range(max_num_training_steps):
-    x_batch, y_batch = mnist.train.next_batch(batch_size)
+    # Main training loop
+    for ii in range(max_num_training_steps):
+        x_batch, y_batch = mnist.train.next_batch(batch_size)
 
-    # Compute Adversarial Perturbations
-    start = timer()
-    x_batch_adv = attack.perturb(x_batch, y_batch, sess)
-    end = timer()
-    training_time += end - start
+        # Compute Adversarial Perturbations
+        start = timer()
+        x_batch_adv = attack.perturb(x_batch, y_batch, sess)
+        end = timer()
+        training_time += end - start
 
-    nat_dict = {model.x_input: x_batch,
-                model.y_input: y_batch}
+        nat_dict = {model.x_input: x_batch,
+                    model.y_input: y_batch}
 
-    adv_dict = {model.x_input: x_batch_adv,
-                model.y_input: y_batch}
+        adv_dict = {model.x_input: x_batch_adv,
+                    model.y_input: y_batch}
 
-    # Output to stdout
-    if ii % num_output_steps == 0:
-      nat_acc = sess.run(model.accuracy, feed_dict=nat_dict)
-      adv_acc = sess.run(model.accuracy, feed_dict=adv_dict)
-      print('Step {}:    ({})'.format(ii, datetime.now()))
-      print('    training nat accuracy {:.4}%'.format(nat_acc * 100))
-      print('    training adv accuracy {:.4}%'.format(adv_acc * 100))
-      if ii != 0:
-        print('    {} examples per second'.format(
-            num_output_steps * batch_size / training_time))
-        training_time = 0.0
-    # Tensorboard summaries
-    if ii % num_summary_steps == 0:
-      summary = sess.run(merged_summaries, feed_dict=adv_dict)
-      summary_writer.add_summary(summary, global_step.eval(sess))
+        # Output to stdout
+        if ii % num_output_steps == 0:
+            nat_acc = sess.run(model.accuracy, feed_dict=nat_dict)
+            adv_acc = sess.run(model.accuracy, feed_dict=adv_dict)
+            print('Step {}:    ({})'.format(ii, datetime.now()))
+            print('    training nat accuracy {:.4}%'.format(nat_acc * 100))
+            print('    training adv accuracy {:.4}%'.format(adv_acc * 100))
+            if ii != 0:
+                print('    {} examples per second'.format(num_output_steps * batch_size / training_time))
+                training_time = 0.0
 
-    # Write a checkpoint
-    if ii % num_checkpoint_steps == 0:
-      saver.save(sess,
-                 os.path.join(model_dir, 'checkpoint'),
-                 global_step=global_step)
+        # Tensorboard summaries
+        if ii % num_summary_steps == 0:
+            summary = sess.run(merged_summaries, feed_dict=adv_dict)
+            summary_writer.add_summary(summary, global_step.eval(sess))
 
-    # Actual training step
-    start = timer()
-    sess.run(train_step, feed_dict=adv_dict)
-    end = timer()
-    training_time += end - start
+        # Write a checkpoint
+        if ii % num_checkpoint_steps == 0:
+            saver.save(sess, os.path.join(model_dir, 'checkpoint'), global_step=global_step)
+
+        # Actual training step
+        start = timer()
+        sess.run(train_step, feed_dict=adv_dict)
+        end = timer()
+        training_time += end - start
